a07accc95bf7d3d85b3c5266b4ea379509e3fe29
trungchubao <trungchubao@aromajoin.com>
2020-02-05 14:33:33 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## mDNS Service Browser for Aroma Shooter in Windows
+
+This is an example to scan Aroma Shooter via Wifi by browsing its mDNS service. This demo is written in C#. 
+
+### Prerequisites:
+
+- [Apple Bonjour for Windows]: https://developer.apple.com/bonjour/
+
+### References:
+
+- Mono.Zeroconf v 0.9.0 was used. 
+
+  [More about Zeroconf]: https://www.mono-project.com/archived/monozeroconf/
+
+- This sample was hugely based on MZClient in 
+
+  [Repository]: https://github.com/mono/Mono.Zeroconf
--- mDNS_Browser_Windows/Program.cs
+++ mDNS_Browser_Windows/Program.cs
@@ -1,15 +1,99 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Mono.Zeroconf;
 
 namespace mDNS_Browser_Windows
 {
     class Program
     {
-        static void Main(string[] args)
+        private static bool resolve_shares = true;
+        private static uint @interface = 0;
+        private static AddressProtocol address_protocol = AddressProtocol.Any;
+        private static string domain = "local";
+        private static Dictionary<string, string> serial2IP = new Dictionary<string, string>();
+        private static Dictionary<string, string> name2Serial = new Dictionary<string, string>();
+
+        public static int Main(string[] args)
         {
+            string type = "_http._tcp";
+            ArrayList services = new ArrayList();
+
+            Console.WriteLine("Hit ^C when you're bored waiting for responses.");
+            Console.WriteLine();
+
+            // Listen for events of some service type
+            ServiceBrowser browser = new ServiceBrowser();
+            browser.ServiceAdded += OnServiceAdded;
+            browser.ServiceRemoved += OnServiceRemoved;
+            browser.Browse(@interface, address_protocol, type, domain);
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+
         }
+
+        private static void OnServiceAdded(object o, ServiceBrowseEventArgs args)
+        {
+            if (resolve_shares)
+            {
+                Thread thread = new Thread(() => resolve(args));
+                thread.Start();
+            }
+        }
+
+        private static void resolve(ServiceBrowseEventArgs args)
+        {
+            args.Service.Resolved += OnServiceResolved;
+            args.Service.Resolve();
+        }
+
+        private static void OnServiceRemoved(object o, ServiceBrowseEventArgs args)
+        {
+            if (name2Serial.ContainsKey(args.Service.Name))
+            {
+                Console.WriteLine("Disconnected Aroma Shooter: {0} at IP: {1}", name2Serial[args.Service.Name], serial2IP[name2Serial[args.Service.Name]]);
+                serial2IP.Remove(name2Serial[args.Service.Name]);
+                name2Serial.Remove(args.Service.Name);
+            }
+        }
+
+        private static void OnServiceResolved(object o, ServiceResolvedEventArgs args)
+        {
+            IResolvableService service = o as IResolvableService;
+
+            string ip = service.HostEntry.AddressList[0].ToString();
+            string serial = "";
+
+            ITxtRecord record = service.TxtRecord;
+            int record_count = record != null ? record.Count : 0;
+            if (record_count > 0)
+            {
+                for (int i = 0, n = record.Count; i < n; i++)
+                {
+                    TxtRecordItem item = record.GetItemAt(i);
+                    if (item.Key == "device")
+                    {
+                        serial = item.ValueString;
+                        Regex regexASNSerial = new Regex(@"AS\w{8}");
+                        Match match;
+
+                        match = regexASNSerial.Match(serial);
+
+                        if (match.Success)
+                        {
+                            name2Serial.Add(args.Service.Name, serial);
+                            serial2IP.Add(serial, ip);
+                            Console.WriteLine("Found Aroma Shooter: serial = {0}, IP = {1}.", serial, ip);
+                        }
+                    }                        
+                }
+            }
+        }
+
     }
 }
